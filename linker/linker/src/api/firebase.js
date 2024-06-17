9ff40236dd8679b9ec49c8c104252b8754fb0623
danielvaughann <151848521+danielvaughann@users.noreply.github.com>
2024-06-17 14:24:22 +0100

    Update firebase.js

--- linker/linker/src/api/firebase.js
+++ linker/linker/src/api/firebase.js
@@ -8,13 +8,7 @@ import { getStorage } from "firebase/storage";
 // Your web firebase's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDyguOhVlPLKwlnKpp4SKgn5fNM_NVy6zA",
-    authDomain: "linker-6027f.firebasefirebase.com",
-    projectId: "linker-6027f",
-    storageBucket: "linker-6027f.firebasespot.com",
-    messagingSenderId: "734901639496",
-    appId: "1:734901639496:web:52de8e006ce1e7b462fb7d",
-    measurementId: "G-079K0KHJ6R"
+   //private information
 };
 
 // Initialize Firebase
